f15fd5bb2b6e60f9ffe843c5024550896c6e8d98
Ramalho-cyber <gamalho455@gmail.com>
2026-08-04 09:26:17 -0300

    Add files via upload

---
+++ Exercicio Triangulo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int base, altura;
+	float area;
+	
+	base = 10;
+	altura = 20;
+	
+	area = (base*altura)/2;
+	
+	printf("A area do Trinagulo Retangulo de Base = %d e Altura = %d e %f", base, altura, area);
+	
+	
+	return 0;
+}
+
