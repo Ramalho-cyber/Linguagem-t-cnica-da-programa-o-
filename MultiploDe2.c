5c57d5b042bd8299087868359a27d096fd27031f
Ramalho-cyber <gamalho455@gmail.com>
2026-08-25 09:22:54 -0300

    Create MultiploDe2.c

---
+++ MultiploDe2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	int a, b, r;
+	printf("Entre com os valores do problema: ");
+	scanf("%d %d", &a, &b);
+	
+	if(a>0 && b>0){
+		if(a%2 == 0){
+			if(b%2 == 0){
+				printf("são multiplos de 2");
+			}
+			
+		}
+		if(a>b){
+			if(a%b == 0){
+				printf("são multiplos");
+			}else{
+				if(b%a == 0){
+					printf("são multiplos");
+				}
+			}
+		}	
+    }
+    
+	return 0;
+}
