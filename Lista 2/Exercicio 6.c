4f485544cae5d2e9bddd445e72bfaf4776dfc27c
Gabriel D Ramalho <gamalho455@gmail.com>
2026-09-15 23:25:50 -0300

    Update Exercicio 6.c

--- Lista 2/Exercicio 6.c
+++ Lista 2/Exercicio 6.c
@@ -10,13 +10,5 @@ int main(int argc, char *argv[]) {
 	printf(" o numero %d, seu antecessor %d e seu sucessor %d", n, n-1, n+1);
 	
 	
-	int a, b, c, maiorTemp, maior;
-	printf("insira tres valores:");
-	scanf("%d, %d, %d", &a, &b, &c);
-	
-
-	maiorTemp = ((a+b+abs(a-b))/2);
-	printf("o maior entre |%d|%d|%d| = %d", a,b,c, maiorTemp);
-	
 	return 0;
 }
